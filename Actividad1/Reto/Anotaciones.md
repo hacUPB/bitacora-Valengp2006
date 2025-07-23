466af8ef152fd258c41273b7a2c7bd274f3e1881
Valengp2006 <valentinagp2006@gmail.com>
2025-07-23 17:21:13 -0500

    Actualización reto

--- Actividad1/Reto/Anotaciones.md
+++ Actividad1/Reto/Anotaciones.md
@@ -1,8 +1,12 @@
-Para poder comenzar a realizar los ejercicios tuve que devolverme a revisar el material dado en clase, ya que tuve complicaciones para entender el tema desde el principio, pero luego de retroalimentar la información y resolver algunso ejemplos logré realizar el reto.
+# Anotaciones reto actividad 1:
+
+- Para poder comenzar a realizar los ejercicios tuve que devolverme a revisar el material dado en clase, ya que tuve complicaciones para entender el tema desde el principio, pero luego de retroalimentar la información y resolver algunso ejemplos logré realizar el reto.
+
+- Además, fue necesaria la elaboración de un cuadro en donde se encuentran las ramas utilizadas en el lenguaje Assembly junto con su condición de salto y significado para una mejor comprensión del tema.
 
 ### Branching instructions:
 
-|  Abreviación |  Condición de salto  |            Significado            |
+|    Ramas    |  Condición de salto  |            Significado            |
 |-------------|-----------------------|-----------------------------------|
 |    `JGT`    |       `comp > 0`      | Salta si el resultado es positivo |
 |    `JEQ`    |      `comp == 0`      |   Salta si el resultado es cero   |
@@ -12,3 +16,4 @@ Para poder comenzar a realizar los ejercicios tuve que devolverme a revisar el m
 |    `JLE`    |      `comp <= 0`      |    Salta si es cero o negativo    |
 |    `JMP`    |    (sin condición)    |     Salta incondicionalmente      |
 
+- Para la solución de los ejercicios relacionados con **variables** fue necesaria también la revisión de la presentación de PowerPoint en donde esta la información del curso, además de algunos tutoriales de YouTube pertenecientes al curso de Nand2Tetris.
---
+++ Actividad1/Reto/Ejercicios_8-9.asm
@@ -0,0 +1,61 @@
+//8. Considera el siguiente programa:
+
+    @var1
+    D=M
+    @var2
+    D=D+M
+    @var3
+    M=D
+
+    //a) ¿Qué hace este programa?
+    //b) ¿En que posición de la memoria está var1, var2 y var3?
+    //¿Por qué en esas posiciones?
+
+    //Respuestas:
+
+    // a) Este programa suma el valor almacenado en la posición de memoria `var1` con el valor almacenado en `var2` y guarda el resultado en `var3`.
+    // b) La posición de memoria de 'var1' se encuentra en la posición 16 de la RAM, ya que los primeros 15 registros estan reservados para la memoria, de igual forma 'var2' y 'var3' ocupan las posiciones 17 y 18 respectivamente.
+
+//9. Considera el siguiente programa:
+    
+   i=1
+   sum=0
+   sum=sum+i
+   i=i+1
+
+   //La traducción al lenguaje ensamblador del programa anterior es:
+
+   // i = 1
+    @i
+    M=1
+    // sum = 0
+    @sum
+    M=0
+    // sum = sum + i
+    @i
+    D=M
+    @sum
+    M=D+M
+    // i = i + 1
+    @i
+    D=M+1
+    @i
+    M=D
+
+    //a) ¿Qué hace este programa?
+    //b) ¿En qué parte de la memoria RAM está la variable i y sum? ¿Por qué en esas posiciones?
+    //c) Optimiza esta parte del código para que use solo dos instrucciones:
+        // i = i + 1
+        @i
+        D=M+1
+        @i
+        M=D
+
+    // Respuestas:
+
+    // a) Este programa inicializa la variable `i` en 1 y la variable `sum` en 0, luego suma el valor de `i` a `sum`, y finalmente incrementa `i` en 1.
+    // b) La variable `i` se encuentra en la posición 16 de la RAM y `sum` en la posición 17. Estas posiciones son elegidas porque las primeras 15 posiciones de la RAM están reservadas para el sistema, por lo que las variables de usuario comienzan a partir de la posición 16.
+    // c) La optimización del código para que use solo dos instrucciones es la siguiente:
+        @i
+        M=M+1
+
