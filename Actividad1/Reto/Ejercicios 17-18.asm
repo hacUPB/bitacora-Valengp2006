b04fad79c5568a6c781846b5d432264cd6dca4e7
Valengp2006 <valentinagp2006@gmail.com>
2025-07-24 18:43:52 -0500

    actualizacion final (1)

---
+++ Actividad1/Reto/Ejercicios 17-18.asm
@@ -0,0 +1,10 @@
+//17. Implementa en lenguaje ensamblador:
+    
+    if ( (D - 7) == 0) goto a la instrucción en ROM[69]
+
+    @7
+    D=D-A
+    @69
+    D;JEQ 
+   
+//18. Utiliza esta herramienta para dibujar un bitmap en la pantalla.
---
+++ Actividad1/Reto/Ejercicios 19-20.asm
@@ -0,0 +1,44 @@
+//19. Analiza el siguiente programa en lenguaje de máquina:
+    
+    0100000000000000
+    1110110000010000
+    0000000000010000
+    1110001100001000
+    0110000000000000
+    1111110000010000
+    0000000000010011
+    1110001100000101
+    0000000000010000
+    1111110000010000
+    0100000000000000
+    1110010011010000
+    0000000000000100
+    1110001100000110
+    0000000000010000
+    1111110010101000
+    1110101010001000
+    0000000000000100
+    1110101010000111
+    0000000000010000
+    1111110000010000
+    0110000000000000
+    1110010011010000
+    0000000000000100
+    1110001100000011
+    0000000000010000
+    1111110000100000
+    1110111010001000
+    0000000000010000
+    1111110111001000
+    0000000000000100
+    1110101010000111
+    
+    //- ¿Qué hace este programa?
+
+    //•	Usa RAM[16] como índice para pantalla (i).
+	//•	Revisa constantemente si hay una tecla presionada.
+	//•	Si no hay tecla presionada, continúa escribiendo en la pantalla secuencialmente.
+	//•	Si hay tecla presionada, realiza una acción diferente.
+	//•	Después de esa acción, regresa al bucle.
+
+//20. Implementa un programa en lenguaje ensamblador que dibuje el bitmap que diseñaste en la pantalla solo si se presiona la tecla “d”.
